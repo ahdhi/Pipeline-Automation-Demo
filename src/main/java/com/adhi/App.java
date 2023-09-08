5f927db554b389cfe19296916c615f3a8054d89c
ahdhi <personal.adhithyan@gmail.com>
2023-09-09 01:41:49 +1000

    Initial commit

---
+++ src/main/java/com/adhi/App.java
@@ -0,0 +1,13 @@
+package com.adhi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!, This is a sample Maven Program by Adhi" );
+    }
+}
