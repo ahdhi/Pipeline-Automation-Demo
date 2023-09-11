45041b06d4ad357a32cad25669d6f46558e24f75
ahdhi <132062086+ahdhi@users.noreply.github.com>
2023-09-12 00:41:34 +1000

    Update integration.sh

--- integration.sh
+++ integration.sh
@@ -5,7 +5,6 @@ export API_BASE_URL="ec2-13-211-229-239.ap-southeast-2.compute.amazonaws.com"
 
 # Perform integration tests
 echo "Running Integration Tests..."
-
 # Example: Send GET request to retrieve data
 response=$(curl -s "${API_BASE_URL}/api/endpoint")
 if [ $? -eq 0 ]; then
