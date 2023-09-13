e6aa823a953a9fd69a78bb56541b4a700cdb061f
ahdhi <132062086+ahdhi@users.noreply.github.com>
2023-09-13 16:27:29 +1000

    Update integration.sh

--- integration.sh
+++ integration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# The base URL of API
-export API_BASE_URL="ec2-13-211-229-239.ap-southeast-2.compute.amazonaws.com"
+# Define the base URL of your API 
+export API_BASE_URL="ec2-13-211-229-239.ap-southeast-2.compute.amazonaws.com:8080"
 
 # Perform integration tests
 echo "Running Integration Tests..."
+
 # Example: Send GET request to retrieve data
 response=$(curl -s "${API_BASE_URL}/api/endpoint")
 if [ $? -eq 0 ]; then
